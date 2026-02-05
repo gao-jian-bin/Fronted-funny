f8d61ec84e1e6f314f89dbce79dc0b315fba75e7
gao-jian-bin <1820474883@qq.com>
2026-02-05 22:37:33 +0800

    feat: add my new front-end page

---
+++ 01/01.js
@@ -0,0 +1,22 @@
+// let a = 0;
+// const b = 1;
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// console.log(add(a, b))
+
+// let arr = [1, 121, 3]
+// let n = arr.length;
+// for (let i = 0; i < n; ++i) {
+//     console.log(arr[i]);
+// }
+let name = 'zhangsan'
+console.log(`hello, ${name}`)
+console.log(`fuck you, ${name}`)
+let i = 1
+while (i < 100) {
+    console.log('infinite loop');
+    console.log(`${i++}`)
+}
